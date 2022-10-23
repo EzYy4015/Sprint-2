a021d5349730717a8a8eb50832e9e1c709d09781
Bong0530 <114717011+Bong0530@users.noreply.github.com>
2022-10-23 20:26:57 +0800

    Add files via upload

---
+++ Bong0530/include/navigation.php
@@ -0,0 +1,30 @@
+<?php
+    echo
+        '<div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+        <!-- <img src="images/logo4.png"> -->
+        <div class="sidenav-box">
+            <a href="#">Home</a>
+            <a href="#">Bookings</a>
+            <a href="#">Products</a>
+            <a href="#">Forum</a>
+            <a href="#">Guide</a>
+        </div>
+
+        <div class="sidenav-contact">
+            <p>Our Socials</p>
+            <div class="sidenav-contact-logos">
+                <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+
+        </div>
+        
+        <div id="mySideMsg" class="sidemsg">
+        <span class="opennav" onclick="openNav()"><i class="fa fa-bars" aria-hidden="true"></i></span>
+            <img src="images/sidelogo.png">
+        </div>'
+?>
