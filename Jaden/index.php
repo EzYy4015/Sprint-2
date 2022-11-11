2a411f1e9552e884d79ba3134bd76114ce0aef1c
Jad <110762227+Pinkslushie@users.noreply.github.com>
2022-11-11 21:46:53 +0800

    Update index.php

--- Jaden/index.php
+++ Jaden/index.php
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="script/script.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="script/livechat-vis.js"></script>
 
 </head>
@@ -17,8 +18,13 @@
 
     <?php 
     include ('include/navigation.php'); 
-    include ('include/connection.php'); 
-    
+    include ('include/connection.php');
+
+    if(!isset($_SESSION['userid']))
+    {
+        $_SESSION['userid'] = 0;
+    }
+
     $getbanner = "SELECT * FROM banners WHERE bannerVisible = '1' LIMIT 1";
     $querybanner = mysqli_query($con, $getbanner);
     $resultsbanner = mysqli_fetch_array($querybanner);
@@ -33,31 +39,166 @@
 
     <div id="chat-box" class="chat-box">
         <div class="chat-box-close-button">
-            <p> Live Chat Support </p>
-            <span onclick="closeChat()"><i class="fa fa-window-close" aria-hidden="true"></i></span>
+            <div class="chat-box-close-button-left">
+                <p> Live Chat Support </p>
+            </div>
+            <div class="chat-box-close-button-right">
+                <span onclick="closeChat()"><i class="fa fa-window-close" aria-hidden="true"></i></span>
+            </div>
         </div>
 
+        <p id="success-text"><i>You have successfully connected with an agent... Say hello!</i></p>
         <!-- Main Content of Messaging -->
-        <div class="chat-box-message-admin">
-            <div class="chat-box-message-admin-left">
-                <i class='fas fa-headset'></i>
+        <div id="admin-icon" class="chat-box-adm-icon">
+            <i class='fas fa-headset'></i>
+        </div>
+        <div id="faq-intro" class="chat-container-admin-intro">
+            <div class="chat-message-admin">
+                <p>Hello! How may we help you? Below are the most frequently asked questions (FAQs), </p>
             </div>
-            <div class="chat-box-message-admin-right">
-                <p>admin</p>
+            <div class="chat-message-admin">
+                <a href="#" onclick="openFAQ1()"><p>1. How do I check my bookings?</p></a>
+                <a href="#" onclick="openFAQ2()"><p>2. I want to stop receiving notifications!</p></a>
+                <a href="#" onclick="connectAgent(<?php echo $_SESSION['loggedin']; ?>)"><p>I want to talk to a live agent instead.</p></a>
             </div>
         </div>
 
-        <div class="chat-box-message-visitor">
-            <div class="chat-box-message-visitor-left">
-                <p>visitor</p>
+
+        
+        <div id="faq-faq1" class="chat-container-admin-intro">
+            <span onclick="mainFAQ()"><i class="fa fa-arrow-left" aria-hidden="true"> Back</i></span>
+            <div class="chat-message-admin">
+                <p>To check your bookings, you may check it <a href="#"> here </a> </p>
             </div>
-            <div class="chat-box-message-visitor-right">
-                <i class="fa fa-user" aria-hidden="true"></i>
+            <div class="chat-message-admin">
+                <p>Doesn't help? Try contacting one of our live agents!</p>
+            </div>
+        </div>
+
+
+
+        <div id="faq-faq2" class="chat-container-admin-intro">
+            <span onclick="mainFAQ()"><i class="fa fa-arrow-left" aria-hidden="true"> Back</i></span>
+            <div class="chat-message-admin">
+                <p>To stop receiving notifications, disable it from your profile settings. </p>
+            </div>
+            <div class="chat-message-admin">
+                <p>Doesn't help? Try contacting one of our live agents!</p>
+            </div>
+        </div>
+
+
+
+
+
+        <div id="connect-chat" class="chat-container-admin-intro">
+            <div class="chat-message-admin">
+                <p>Attempting to connect you with an agent...</p>
             </div>
         </div>
         
 
-        <div class="chat-box-message-bottom">
+
+        <p id="form-fill-text"><i>Since you are not logged in, complete this form to chat with an agent.</i></p>
+        <p id="outside-working-hours"><i>The chat is currently outside its working hours. Alternatively, you may submit this form or wait for our working hours. <br/>(9AM - 5PM)</i></p>
+        <div id="show-form" class="form-live-chat">
+            <form name="submit-inquiry" id="submit-inquiry" method="POST">
+                <table>
+                    <tr>
+                        <th><label for="name">Name: </label></th>
+                    </tr>   
+
+                    <tr>
+                        <td><input name="name" id="name" type="text"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="email">Email: </label></th>
+                    </tr>   
+
+                    <tr>
+                        <td><input name="email" id="email" type="email"></td>
+                    </tr>
+
+                    <tr>
+                        <th><label for="phone-no">Phone Number: </label></th>
+                    </tr>   
+
+                    <tr>
+                        <td><input name="phone-no" id="phone-no" type="text"></td>
+                        <td><input hidden name="user-id" id="userid" type="text" value=<?php echo $_SESSION['userid']; ?>></td>
+                    </tr>
+
+                    <tr>
+                        <th><label for="inquiry">Your Inquiry: </label></th>
+                    </tr>   
+
+                    <tr>
+                        <td><textarea id="inquiry" name="inquiry" rows="5" cols="40" type = "text"></textarea></td>
+                    </tr>
+                </table>
+                <input type="submit" name="submit" id="live-chat-form-submit-button">
+            </form>
+        </div>
+        <script>
+            $(function () {
+            $('#submit-inquiry').validate();
+
+            $('#submit-inquiry').on('submit', function(e){
+                e.preventDefault();
+
+                // Collect the data.
+                let dataString = $(this).serialize();
+
+                // Send data.
+                $.ajax({
+                // type: 'POST',
+                type: $form.attr('method'),
+                //url: 'include/submit-form.php',
+                url: $form.attr('action'),
+                data: dataString,
+                success: function(){
+                    alert('lol');
+                    checkingFormProcess();
+                }
+                })
+            });
+            });
+        </script>
+
+        <div id="form-submit-success" class="form-submit-success">
+            <i class="fas fa-check-circle"></i>
+            <p>Your request has been received. We shall revert to you within 48 hours.</p>
+        </div>
+
+        <!--
+        <div class="chat-box-vis-icon">
+            <i class="fa fa-user" aria-hidden="true"></i>
+        </div>
+        <div class="chat-container-visitor">
+            <div class="chat-message-visitor">
+                <p>visitor</p>
+            </div>
+            <div class="chat-message-visitor">
+                <p>visitor</p>
+            </div>
+        </div>
+        
+
+        <div class="chat-box-adm-icon">
+            <i class='fas fa-headset'></i>
+        </div>
+        <div class="chat-container-admin">
+            <div class="chat-message-admin">
+                <p>Hello! How may we help you? Below are the most frequently asked questions (FAQs), </p>
+            </div>
+            <div class="chat-message-admin">
+                <p>admin</p>
+            </div>
+        </div>
+        -->
+        
+
+        <div id="send-message-box" class="chat-box-message-bottom">
             <form action="#">
                 <input type="text" id="message" name="message" placeholder="Hello!">
                 <i class='fas fa-location-arrow'></i>
